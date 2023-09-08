90cf65356e19c134a4ec388d96d560f8113ecb60
reda ahmed <reda.ahmed.2032@gmail.com>
2023-09-08 21:57:19 +0300

    add App Assets

---
+++ lib/core/resources/app_assets.dart
@@ -0,0 +1,13 @@
+const String imagePath = "assets/images";
+const String jsonPath = "assets/json";
+
+class AppImageAssets {
+//  static const String splashLogo = "$imagePath/splash_logo.png";
+}
+
+class JsonAssets {
+  // static const String loading = "$JSON_PATH/loading.json";
+  // static const String error = "$JSON_PATH/error.json";
+  // static const String empty = "$JSON_PATH/empty.json";
+  // static const String success = "$JSON_PATH/success.json";
+}
